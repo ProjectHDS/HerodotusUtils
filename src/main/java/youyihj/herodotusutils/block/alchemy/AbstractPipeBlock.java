f352fbd6b12e226ea7b3cdc35a55b03b79e19e91
youyihj <youyi580@qq.com>
2022-01-30 00:53:35 +0800

    pipes are not full cubes

--- src/main/java/youyihj/herodotusutils/block/alchemy/AbstractPipeBlock.java
+++ src/main/java/youyihj/herodotusutils/block/alchemy/AbstractPipeBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import youyihj.herodotusutils.alchemy.IHasAlchemyFluid;
 import youyihj.herodotusutils.alchemy.IPipe;
@@ -49,6 +50,17 @@ public abstract class AbstractPipeBlock extends PlainBlock {
     @Override
     @SuppressWarnings("deprecation")
     public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isNormalCube(IBlockState state, IBlockAccess world, BlockPos pos) {
+        return false;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public boolean isFullCube(IBlockState state) {
         return false;
     }
 
