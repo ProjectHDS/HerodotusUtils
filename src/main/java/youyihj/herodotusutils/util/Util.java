0e5776fc561ca92b2c166205d0dbd7b320879b36
youyihj <youyi580@qq.com>
2022-02-04 11:39:53 +0800

    remove unused stuff

--- src/main/java/youyihj/herodotusutils/util/Util.java
+++ src/main/java/youyihj/herodotusutils/util/Util.java
@@ -4,23 +4,15 @@ import crafttweaker.api.data.DataMap;
 import crafttweaker.api.data.IData;
 import it.unimi.dsi.fastutil.ints.IntCollection;
 import it.unimi.dsi.fastutil.ints.IntIterator;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import org.apache.commons.lang3.ArrayUtils;
-import youyihj.herodotusutils.HerodotusUtils;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.items.CapabilityItemHandler;
+import org.apache.commons.lang3.ArrayUtils;
 
 import javax.annotation.Nullable;
 import java.util.HashMap;
@@ -65,10 +57,6 @@ public final class Util {
         return Optional.ofNullable(world.getTileEntity(pos));
     }
 
-    public static FluidStack getDefaultFluidStack(Fluid fluid) {
-        return new FluidStack(fluid, 1000);
-    }
-
     public static <T> T getCycledNextElement(T[] array, T thisElement) {
         int index = ArrayUtils.indexOf(array, thisElement);
         if (index == ArrayUtils.INDEX_NOT_FOUND) {
@@ -97,41 +85,5 @@ public final class Util {
                         InventoryHelper.spawnItemStack(worldIn, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, itemHandler.getStackInSlot(i).copy());
                     }
                 });
-    }
-
-    @SideOnly(Side.CLIENT)
-    public static void renderItem(Minecraft minecraft, int x, int y, boolean isInput) {
-        bindJeiTexture(minecraft);
-        if (isInput)
-            Gui.drawModalRectWithCustomSizedTexture(x - 1, y - 1, 0, 0, 18, 18, 256, 256);
-        else
-            Gui.drawModalRectWithCustomSizedTexture(x - 5, y - 5, 80, 0, 27, 27, 256, 256);
-    }
-
-    @SideOnly(Side.CLIENT)
-    public static void renderArrow(Minecraft minecraft, int x, int y, int direction) {
-        bindJeiTexture(minecraft);
-        switch (direction) {
-            case 0:
-                Gui.drawModalRectWithCustomSizedTexture(x, y, 36, 0, 22, 15, 256, 256);// right
-                break;
-            case 1:
-                Gui.drawModalRectWithCustomSizedTexture(x, y, 36, 16, 22, 15, 256, 256);// left
-                break;
-            case 2:
-                Gui.drawModalRectWithCustomSizedTexture(x, y, 64, 0, 15, 22, 256, 256);// up
-                break;
-            case 3:
-                Gui.drawModalRectWithCustomSizedTexture(x, y, 64, 32, 15, 22, 256, 256);// down
-                break;
-            default:
-                HerodotusUtils.logger.error("Direction is not supported and you shouldn't goto in here");
-        }
-    }
-
-    @SideOnly(Side.CLIENT)
-    private static void bindJeiTexture(Minecraft minecraft) {
-        GlStateManager.enableAlpha();
-        minecraft.getTextureManager().bindTexture(HerodotusUtils.rl("textures/gui/jei/jei_default.png"));
     }
 }
