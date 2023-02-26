e2dd78df9de73faa2203ef8832aeb2b982443711
Gary Bryson Luis Jr <garybluis@outlook.com>
2023-02-26 20:58:58 +0800

    Mixin LP

---
+++ src/main/java/projecthds/herodotusutils/mixins/mods/logisticspipes/MixinHardcodedText.java
@@ -0,0 +1,33 @@
+package projecthds.herodotusutils.mixins.mods.logisticspipes;
+
+import logisticspipes.items.ItemUpgrade;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import network.rs485.logisticspipes.util.TextUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.List;
+
+@Mixin(value = ItemUpgrade.class, remap = false)
+public class MixinHardcodedText {
+    /**
+     * @author Gary Bryson Luis Jr.
+     * @reason Provide translation key for some connective phrases.
+     * */
+    @Overwrite
+    @SideOnly(Side.CLIENT)
+    private String join(List<String> join) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < join.size() - 2; i++) {
+            builder.append(TextUtil.translate(ItemUpgrade.SHIFT_INFO_PREFIX + join.get(i)));
+            builder.append(TextUtil.translate(ItemUpgrade.SHIFT_INFO_PREFIX + "connect_symbol"));
+        }
+        if (join.size() > 1) {
+            builder.append(TextUtil.translate(ItemUpgrade.SHIFT_INFO_PREFIX + join.get(join.size() - 2)));
+            builder.append(TextUtil.translate(ItemUpgrade.SHIFT_INFO_PREFIX + "and"));
+        }
+        builder.append(TextUtil.translate(ItemUpgrade.SHIFT_INFO_PREFIX + join.get(join.size() - 1)));
+        return builder.toString();
+    }
+}
