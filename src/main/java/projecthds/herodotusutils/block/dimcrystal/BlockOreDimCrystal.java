f282a1466abdeeab68304a4162c35e9ad5ec5d00
cneicy <im@crash.work>
2025-07-07 21:32:07 +0800

    feat:使维度水晶不可破坏不可推动

--- src/main/java/projecthds/herodotusutils/block/dimcrystal/BlockOreDimCrystal.java
+++ src/main/java/projecthds/herodotusutils/block/dimcrystal/BlockOreDimCrystal.java
@@ -1,8 +1,13 @@
 package projecthds.herodotusutils.block.dimcrystal;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.EnumPushReaction;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import projecthds.herodotusutils.block.PlainBlock;
 
 import java.util.ArrayList;
@@ -29,6 +34,17 @@ public class BlockOreDimCrystal extends PlainBlock {
     BlockOreDimCrystal(String ore) {
         super(Material.GLASS, NAME + "_" + ore.toLowerCase(Locale.ENGLISH));
         content = ore.toLowerCase(Locale.ENGLISH);
+        this.blockHardness = -1;
+    }
+
+    @Override
+    public EnumPushReaction getPushReaction(IBlockState state) {
+        return EnumPushReaction.BLOCK;
+    }
+
+    @Override
+    public boolean canHarvestBlock(IBlockAccess world, BlockPos pos, EntityPlayer player) {
+        return false;
     }
 
     public static class Item extends ItemBlock {
@@ -36,4 +52,5 @@ public class BlockOreDimCrystal extends PlainBlock {
             super(block);
         }
     }
+
 }
--- src/main/java/projecthds/herodotusutils/config/HDSUConfig.java
+++ src/main/java/projecthds/herodotusutils/config/HDSUConfig.java
@@ -11,14 +11,7 @@ public class HDSUConfig {
     public static int MinUpdatesForQuartz = 42;// 锂石英生成的最小更新次数
     public static int MaxUpdatesForQuartz = 52;// 锂石英生成的最大更新次数
     public static int PowderDropCountForQuartz = 7;// 粉末掉落数量
-
-    public static int UpdateRateThresholdForQuartz = 5;// 每秒最大允许更新次数
-    public static int RateCheckIntervalForQuartz = 20;// 检测间隔（20 ticks = 1秒）
-
-    public static int DebounceIntervalForQuartz = 1;// 消抖间隔（单位：tick）
-    public static int MaxBurstUpdatesForQuartz = 1;// 允许的最大突发更新数
-    public static boolean OverloadExplosionBreakBlock = false;
-
+    public static int BlockUnstableLeadDimFragmentActivationDelay = 5;
     public static boolean nullpinterWelcomeEnabled = true;
 
     static {
